2041364e2b0e110a3a96886b77ff4a971c747076
mago mahi <maheshhonnamurthy099@gmail.com>
2022-11-11 04:44:17 +0530

    [Mahesh]Add: Eqaulity of two lines

--- LineComparisionProblem/Program.cs
+++ LineComparisionProblem/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using LineComparisionProblem;
+using System;
+
+namespace LineComparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to line comparision problem statement ");
+            CalculateLength calculate1 = new CalculateLength(1, 2, 3, 4);
+            double length1 = calculate1.Calculate();
+            CalculateLength calculate2 = new CalculateLength(1, 2, 4, 4);
+            double length2 = calculate2.Calculate();
+            if (length1.Equals(length2))//length1==length2
+                Console.WriteLine("Both Lines are Equals");
+            else
+                Console.WriteLine("Both Lines are Not Equals");
+        }
+    }
+}
